20f2f9d3f555ef8b50570840ab0e4d2277188821
Nahuel Garcia <nhlgrc@hotmail.com>
2025-03-06 14:12:08 -0300

    commit desde saico

--- src/main/java/org/ngarcia/webapp/configs/ProducerResources.java
+++ src/main/java/org/ngarcia/webapp/configs/ProducerResources.java
@@ -1,25 +1,26 @@
 package org.ngarcia.webapp.configs;
 
+import jakarta.annotation.Resource;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Named;
+import java.sql.*;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+import javax.naming.*;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class ProducerResources {
 
+   @Resource(name="jdbc/mysqlDB")
+   private DataSource ds;
+   
    @Produces
    @RequestScoped
    @Named("conn")
    private Connection beanConnection() throws NamingException, SQLException {
-      Context initContext = new InitialContext();
-      Context envContext = (Context) initContext.lookup("java:/comp/env");
-      DataSource ds = (DataSource) envContext.lookup("jdbc/mysqlDB");
+      //Context initContext = new InitialContext();
+      //Context envContext = (Context) initContext.lookup("java:/comp/env");
+      //DataSource ds = (DataSource) envContext.lookup("jdbc/mysqlDB");
       return ds.getConnection();
    }
 }
